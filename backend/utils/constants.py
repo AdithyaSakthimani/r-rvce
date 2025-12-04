f8268b82517f5563d1217c2333717da9967a3e60
seshasai-1827 <seshasaichillara@gmail.com>
2025-12-05 01:09:40 +0530

    conflicts resolved

--- backend/utils/constants.py
+++ backend/utils/constants.py
@@ -5,3 +5,4 @@ load_dotenv()
 MONGODB_URI = os.getenv("MONGODB_URI")
 DATABASE_NAME = os.getenv("DATABASE_NAME")
 COMPILE_URL = "https://emkc.org/api/v2/piston/execute"
+HF_TOKEN = os.getenv("HF_TOKEN")
