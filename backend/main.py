f09f0291e6ede3935f8f9b3b613e0b0954e84a6d
AdithyaSakthimani <adithyasakthimani@gmail.com>
2025-12-05 02:27:49 +0530

    added behavior analysis

--- backend/main.py
+++ backend/main.py
@@ -2,8 +2,10 @@
 import flask
 from controllers.login import login_bp
 from controllers.compiler import compiler_bp
+from controllers.behavior import behavior_bp
 app = flask.Flask(__name__)
 app.register_blueprint(login_bp, url_prefix="/auth")
 app.register_blueprint(compiler_bp, url_prefix="/compiler")
+app.register_blueprint(behavior_bp, url_prefix="/behavior")
 if __name__=="__main__":
     app.run(debug = True)
